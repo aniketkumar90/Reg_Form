165b88c60f9bc5b62d1fec74d375f81603e49e62
aniketkumar90 <aniketkumar2018ak@gmail.com>
2025-06-15 22:41:33 +0530

    change in index file

--- Script.js
+++ Script.js
@@ -1,5 +1,36 @@
-let s1=document.querySelector(sub);
- 
- for(c in s1){
-    console.log(c.option.value);
- }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const selects = document.querySelectorAll("select");
+
+    selects.forEach(select => {
+        select.dataset.previous = select.value;
+
+        select.addEventListener("change", () => {
+            updateOptions();
+        });
+    });
+    function updateOptions() {
+        const selectedValues = Array.from(selects)
+            .map(select => select.value)
+            .filter(val => val !== "" && val !== "Select a subject");
+
+        selects.forEach(currentSelect => {
+            const currentValue = currentSelect.value;
+
+            Array.from(currentSelect.options).forEach(opt => {
+                opt.hidden = false;
+            });
+
+            selectedValues.forEach(value => {
+                Array.from(currentSelect.options).forEach(opt => {
+                    if (opt.value === value && opt.value !== currentValue) {
+                        opt.hidden = true;
+                    }
+                });
+            });
+        });
+    }
+
+
+    updateOptions();
+});
