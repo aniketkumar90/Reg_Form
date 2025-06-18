f667d87200a525d235faa937cae8957ec67bff2e
aniketkumar90 <aniketkumar2018ak@gmail.com>
2025-06-18 23:11:26 +0530

    include footer

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php include_once 'header.php';?>
-        <header>
-        <div class="row py-3 border-bottom">
-          
-          <div class="col-sm-4 col-lg-3 text-center text-sm-start">
+<header>
+    <div class="row py-3 border-bottom">
+
+        <div class="col-sm-3 ">
             <div class="main-logo">
-              <a href="index.html">
-                <img src="images/logo.png" alt="logo" class="img-fluid">
-              </a>
+                <a href="index.html">
+                    <img src="images/logo.png" alt="logo" class="img-fluid">
+                </a>
             </div>
-          </div>
-          
-          <div class="col-sm-6 offset-sm-2 offset-md-0 col-lg-5 d-none d-lg-block">
-            <div class="search-bar row bg-light p-2 my-2 rounded-4">
+        </div>
+
+        <div class="col-sm-5">
+           <div class="search-bar row bg-light p-2 my-2 rounded-4">
               <div class="col-md-4 d-none d-md-block">
                 <select class="form-select border-0 bg-transparent">
                   <option>All Categories</option>
@@ -22,52 +22,78 @@
               </div>
               <div class="col-11 col-md-7">
                 <form id="search-form" class="text-center" action="index.html" method="post">
-                  <input type="text" class="form-control border-0 bg-transparent" placeholder="Search ">
+                  <input type="text" class="form-control border-0 bg-transparent" placeholder="Search for more than 20,000 products">
                 </form>
               </div>
               <div class="col-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M21.71 20.29L18 16.61A9 9 0 1 0 16.61 18l3.68 3.68a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.39ZM11 18a7 7 0 1 1 7-7a7 7 0 0 1-7 7Z"></path></svg>
               </div>
             </div>
-          </div>
-          
-          <div class="col-sm-8 col-lg-4 d-flex justify-content-end gap-5 align-items-center mt-4 mt-sm-0 justify-content-center justify-content-sm-end">
-            <div class="support-box text-end d-none d-xl-block">
+        </div>
+
+        <div class="col-sm-4 ">
+           <div class="row  py-1 ">
+            <div class="col-md-5">
+              <div class="support-box text-end d-none d-xl-block">
               <span class="fs-6 text-muted">For Support?</span>
-              <h5 class="mb-0">1234567890</h5>
+              <h5 class="mb-0">+980-34984089</h5>
             </div>
-
-            <ul class="d-flex justify-content-end list-unstyled m-0">
-              <li>
-                <a href="#" class="rounded-circle bg-light p-2 mx-1">
-                  <svg width="24" height="24" viewBox="0 0 24 24"><use xlink:href="#user"></use></svg>
-                </a>
-              </li>
-              <li>
-                <a href="#" class="rounded-circle bg-light p-2 mx-1">
-                  <svg width="24" height="24" viewBox="0 0 24 24"><use xlink:href="#heart"></use></svg>
-                </a>
-              </li>
-              <li class="d-lg-none">
-                <a href="#" class="rounded-circle bg-light p-2 mx-1" data-bs-toggle="offcanvas" data-bs-target="#offcanvasCart" aria-controls="offcanvasCart">
-                  <svg width="24" height="24" viewBox="0 0 24 24"><use xlink:href="#cart"></use></svg>
-                </a>
-              </li>
-              <li class="d-lg-none">
-                <a href="#" class="rounded-circle bg-light p-2 mx-1" data-bs-toggle="offcanvas" data-bs-target="#offcanvasSearch" aria-controls="offcanvasSearch">
-                  <svg width="24" height="24" viewBox="0 0 24 24"><use xlink:href="#search"></use></svg>
-                </a>
-              </li>
-            </ul>
-
-            <div class="cart text-end d-none d-lg-block dropdown">
-              <button class="border-0 bg-transparent d-flex flex-column gap-2 lh-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasCart" aria-controls="offcanvasCart">
-                <span class="fs-6 text-muted dropdown-toggle">Your Cart</span>
-                <span class="cart-total fs-5 fw-bold">$129</span>
-              </button>
             </div>
-          </div>
+            <div class="col-md-3">
+              <div class="row  py-2 ">
+                <div class="col-md-6">
+                  <div class="persion">
+                     
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <img src="images/heart_1.jpeg" alt="logo" class="img-fluid">
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="row  py-3 ">
+                <div class="col-md-6">
+                  <div class="login">
+                    <a href="" >Login</a>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                   <div class="cart">
+                     <img src="images/cart_1.webp" alt="logo" class="img-fluid">
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+        </div>
+
+    </div>
+    <div class="row-py-3 border-bottom">
+        <div class="col-sm-1">
 
         </div>
-      </header>
+        <div class="col-sm-1">
+            <div class="col-md-4 d-none d-md-block">
+                <select class="form-select border-0 bg-transparent">
+                    <option>All Categories</option>
+                    <option>Groceries</option>
+                    <option>Drinks</option>
+                    <option>Chocolates</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-1"></div>
+
+    </div>
+</header>
 <?php include_once 'footer.php';?>
