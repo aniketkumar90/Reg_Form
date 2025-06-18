140d3ed14dff12512d77cd458f7d08fbf6926359
Navin <neelkhanth70@gmail.com>
2025-06-18 23:13:34 +0530

    l

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php include_once 'header.php'; ?>
+<section class="login-section">
+<div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-8">
+       <div class="imgi_back">
+                <img src="images/slide-1.jpg" alt="images">
+                <div class="card">
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, repudiandae!</p>
+                    </div>
+                </div>
+        </div>
+    </div>
+    <div class="col-md-2"></div>
+</div>
+</section>
+<?php include_once 'footer.php'; ?>
+
